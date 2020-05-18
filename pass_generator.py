a7b8e379ae2c562e22bdcd65b0bf5abcbcbadbed
Vedanta <64916545+unworld11@users.noreply.github.com>
2020-05-18 12:25:09 +0530

    Update pass_generator.py

--- pass_generator.py
+++ pass_generator.py
@@ -1,23 +1,21 @@
 import random
 passw = ["1","2","3","4","5","6","7","8","9","0"]
 
-for i in range(32,127):                #generates a list of all possible characters
+for i in range(32,127):
     passw.append(chr(i))
                 
-us_input = int(input("Enter Number of characters :"))         
+us_input = int(input("Enter Number of characters :"))
 
 for i in range(us_input):
-    if us_input < 7:                 #SAFETY
+    if us_input < 7:
         print("Password has to be greater than 7 characters for safety \n : )")
         break
     else:
-        pass_gen = " "                  #Empty string
+        pass_gen = " "
         a = str(random.choice(passw))
         pass_gen += a
         
-    print("Generating Password")
+   
     
     print(pass_gen,end="")    
-                                            
-                
-
+print("\n")
