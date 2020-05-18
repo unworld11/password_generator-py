3bb27709193b3824ce61f33a4d80ea131bfb0b8e
Vedanta <64916545+unworld11@users.noreply.github.com>
2020-05-18 21:22:24 +0530

    update 3

--- pass_generator.py
+++ pass_generator.py
@@ -1,23 +1,45 @@
 import random
 import time
+
+print("PASSWORD GENERATOR 1.0.3 ")
 passw = ["1","2","3","4","5","6","7","8","9","0"]
 
 for i in range(32,127):
     passw.append(chr(i))
-                
-us_input = int(input("Enter Number of characters :"))
-pass_gen = " "
 
-for i in range(us_input):
-    if us_input < 7:
-        print("Password has to be greater than 7 characters for safety \n : )")
-        break
-    else:
-        a = str(random.choice(passw))
-        pass_gen += a
+def pass_generator():
+    
+    us_input = int(input("Enter Number of characters :"))
+    pass_gen = " "
+
+                        
+
+    for i in range(us_input):
+        if us_input < 7:
+            print("Password has to be greater than 7 characters for safety \n : )")
+            break
+        else:
+            a = str(random.choice(passw))
+            pass_gen += a
         
 
-print("Generating Password....")
-time.sleep(2)
-print(pass_gen)    
-print("\n")
+    print("Generating Password....")
+    time.sleep(2)
+    print("Your Password is : ",pass_gen)    
+    print("\n")
+
+    ans=input("Do you want to save our password remembering services ?")
+    if ans == "yes" or ans == "Yes" or ans == "y":
+        user_name = input('Enter name : ')
+        user_service = input("Enter Service (Gmail etc.) : ")
+
+        #Creating a file
+        password = open("password.txt","x")
+        password.close()
+
+        password = open("password.txt","a")
+        password.write(user_service)
+        password.write(pass_gen)
+        password.close()
+
+pass_generator()
